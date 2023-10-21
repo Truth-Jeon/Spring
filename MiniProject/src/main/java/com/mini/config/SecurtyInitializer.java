fa12d8791f8c767fa7f163b60a1f10fd36d90166
Hayoon Jeon <77964989+Truth-Jeon@users.noreply.github.com>
2023-10-21 23:21:07 +0900

    Delete MiniProject directory

--- MiniProject/src/main/java/com/mini/config/SecurtyInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.mini.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurtyInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
